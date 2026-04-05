b4f67340c24dd3f2303e9b84f8f9318396dd5468
Lain Iwakura <lain@iwakura.page>
2026-04-05 20:03:44 +0300

    mr corp

--- src/services/tiktok.rs
+++ src/services/tiktok.rs
@@ -1,3 +1,9 @@
+// Copyright 2026 NotVkontakte LLC (aka Lain)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use anyhow::{Result, anyhow};
 use regex::Regex;
 use reqwest::header::{LOCATION, USER_AGENT};
